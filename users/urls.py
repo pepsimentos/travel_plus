a9a902dd6c016afdbe7842e98320e29d98af6ad2
Raghed <134695616+raghedabu@users.noreply.github.com>
2024-12-06 00:54:58 +0200

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('register/', views.register, name='register'),  # User registration
     path('login/', views.loginPage, name='login'),      # User login
-    path('logout/', views.logout, name='logout'),    # User logout
+    path('logout/', views.logoutUser, name='logout'),    # User logout
     path('confirm-email/<uidb64>/<token>/', views.confirm_email, name='confirm_email'),
     path('bookings/book_package/', views.book_vacation_package,name='book_vacation_package'),  # Booking URL
 ]
